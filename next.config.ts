8f6decdee07ecdb271d9a307fbd89f510f82c1c8
QoyyumO <oyinlolaoyadeyi@gmail.com>
2025-10-25 10:25:59 +0100

    vercel error 2

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverExternalPackages: ['@google/generative-ai', 'groq-sdk'],
+  env: {
+    GOOGLE_AI_STUDIO_API_KEY: process.env.GOOGLE_AI_STUDIO_API_KEY,
+    GROQ_API_KEY: process.env.GROQ_API_KEY,
+  }
 };
 
 export default nextConfig;
